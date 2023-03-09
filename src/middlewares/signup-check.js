9fbc0868fd94f8a42b39ab7ce846a603c870f76a
DONGHAEY <dongheaey@gmail.com>
2023-03-10 00:39:00 +0900

    가입시 사업자 진위여부

--- src/middlewares/signup-check.js
+++ src/middlewares/signup-check.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const {
   BusinessType,
 } = require("../extensions/users-permissions/type/business-type");
@@ -8,8 +9,26 @@ module.exports = (config, { strapi }) => {
       delete ctx.request.body.deliveryBrand;
     }
 
-    //추천인 포인트 기능을 구현할거면 여기서 하자!
-
+    //사업자 진위 확인
+    const { businessId, startDate, name, businessName } = ctx.request.body;
+    console.log(businessId, startDate, name, businessName);
+    const { data } = await axios.post(
+      "https://api.odcloud.kr/api/nts-businessman/v1/validate?serviceKey=Ej2%2BZXeAjR9NujeHJ4UEHtCSxYsT06aq%2BQyLuCqQsnWEb2pjQXF9imj8YGjRiLUSEGyB6xGfTdlcsezKQkgM5w%3D%3D",
+      {
+        businesses: [
+          {
+            b_no: businessId,
+            start_dt: startDate,
+            p_nm: name,
+            b_nm: businessName,
+          },
+        ],
+      }
+    );
+    console.log(data);
+    if (!data.data[0].status) {
+      return ctx.badRequest("사업자 진위 확인 과정에 실패하였습니다");
+    }
     return await next();
   };
 };
