a9aa8a8f94a9b8e8dddda22841a34602be04f44c
DONGHAEY <dongheaey@gmail.com>
2023-03-11 11:05:30 +0900

    fix :: delivery-agency service & controller

--- src/api/delivery-agency/controllers/delivery-agency.js
+++ src/api/delivery-agency/controllers/delivery-agency.js
@@ -10,47 +10,82 @@ module.exports = createCoreController(
   "api::delivery-agency.delivery-agency",
   ({ strapi }) => ({
     service: strapi.service("api::delivery-agency.delivery-agency"),
-    async registerOrEdit(ctx) {
-      return await this.service.registerOrEdit({
-        deliveryAgencyInfo: ctx.request.body,
+    //
+    //배달대행 정보를 등록 및 저장하는 API이다.
+    async registerDeliveryAgencyInfo(ctx) {
+      //이것을 요청하기전 프론트에서 validation 작업을 꼭꼭 해주어야함!!
+
+      //이것을 처음으로 요청하게 되면, 이제 디테일 페이지 및 메인페이지에 뜰 수 있는 자격이 된다.
+      // 처음에는 isRegistered가 false였기 때문에 등록이 다 된 상태가 아닌것이고,
+      // 등록을 하게 되면 isRegistered가 true로 바뀌게 된다.
+      return await this.service.updateInfo({
+        deliveryAgencyInfo: {
+          ...ctx.request.body,
+          isRegistered: true,
+        },
+        files: ctx.request.files,
         user: ctx.state.user,
       });
     },
-    async getByUser(ctx) {
-      return await this.service.getDeliveryAgency({
-        where: {
-          user: ctx.state.user.id,
-        },
-        populate: {
-          deliveryBrand: true,
-          user: true,
-          ratePlan: true,
-          deliveryLocations: true,
-        },
+    //
+    // 자신의 배달대행정보 등록 및 저장 페이지 접근시 반환하는 API이다.
+    async getForRegisterPage(ctx) {
+      const deliveryAgencyInfo = await this.service.getDeliveryAgency({
+        user: ctx.state.user.id,
       });
+      if (!deliveryAgencyInfo) {
+        return this.service.createEmptySheet(ctx.state.user.id);
+      }
+      return deliveryAgencyInfo;
     },
-    async findOne(ctx) {
-      return await strapi.entityService.findOne(
-        "api::delivery-agency.delivery-agency",
-        ctx.params.id,
-        {
-          populate: {
-            //리뷰수, 리뷰 평균
-            deliveryBrand: true,
-            user: true,
-            ratePlan: true,
-            deliveryLocations: true,
-          },
-        }
-      );
+    //
+    /** 배달대행 DETAIL 페이지에 사용하는 API */
+    async getForDetailPage(ctx) {
+      // 모두가 보는 페이지
+      return await this.service.getDeliveryAgency({
+        id: ctx.params.id,
+        isRegistered: true,
+      });
+      // 진행중인 계약수 contractService에 contractsCondition 메서드를 deliveryAgencyId로 호출하여 가져오기
+      // 리뷰수, 리뷰 평균 // 리뷰들 불러오는것은 리뷰컨트롤러에서 자체적으로 처리하기
     },
+
+    async deleteImage(ctx) {
+      const exist = await this.service.getDeliveryAgency({
+        user: ctx.state.user.id,
+      });
+      const imageId = ctx.query.imageId;
+      const imageKey = ctx.query.imageKey;
+      if (
+        !exist?.[imageKey]?.find((d) => {
+          return d.id == imageId;
+        })
+      ) {
+        return ctx.badRequest(
+          "이미지 키를 잘못보냈거나, 자신이 업로드 한 이미지가 아닙니다."
+        );
+      }
+      const imageEntry = await strapi.db.query("plugin::upload.file").delete({
+        where: { id: ctx.query.imageId },
+      });
+      strapi.plugins.upload.services.upload.remove(imageEntry);
+      return "올바르게 이미지가 삭제됨";
+    },
+
+    //
+    //
+    //
+    //
+    //
+    //
+
     async getForMain(ctx) {
       const populateForMainPage = {
         deliveryBrand: true,
         ratePlan: true,
       };
       // 업체명, 영업시간, 평점평균 노출, 인증배찌, 쿠폰 노출 (전국 기준)
-      // 1. 계약순서
+      // 1. 최근 계 약순서
       // 2. 리뷰수와 평점이 높은 순
       // 계약순서 순인 SQL이다.
 
@@ -80,6 +115,7 @@ module.exports = createCoreController(
           contractOrderList.push(deliveryAgencyInfo);
         })
       );
+
       sql = `SELECT avg(reviews.score) as avgScore, delivery_agency_id  as deliveryAgencyId
       FROM reviews, reviews_delivery_agency_links as rd 
       where reviews.id=rd.review_id 
--- src/api/delivery-agency/services/delivery-agency.js
+++ src/api/delivery-agency/services/delivery-agency.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const getDeliveryAgency = require("../middlewares/get-delivery-agency");
-
 /**
  * delivery-agency service
  */
@@ -11,44 +9,76 @@ const { createCoreService } = require("@strapi/strapi").factories;
 module.exports = createCoreService(
   "api::delivery-agency.delivery-agency",
   ({ strapi }) => ({
-    async getDeliveryAgency(condition) {
-      const deliveryAgencyRepository = strapi.db.query(
-        "api::delivery-agency.delivery-agency"
-      );
-      return await deliveryAgencyRepository.findOne(condition);
+    async getDeliveryAgency(where) {
+      const deliveryAgency = await strapi.db
+        .query("api::delivery-agency.delivery-agency")
+        .findOne({
+          populate: {
+            deliveryBrand: true,
+            user: {
+              // 상호명, 개업일자
+              select: ["businessName", "startDate", "id"],
+            },
+            deliveryLocations: {
+              select: ["id", "SIDO", "SIGUNGU", "EUPMEONDONG"],
+            },
+            deliveryAgencyImages: {
+              select: ["id", "url"],
+            },
+            documentOfInsurance: {
+              select: ["id", "url"],
+            },
+          },
+          ratePlan: true,
+          where,
+        });
+      return deliveryAgency;
+    },
+
+    async createEmptySheet(userId) {
+      return await strapi.db
+        .query("api::delivery-agency.delivery-agency")
+        .create({
+          data: {
+            user: userId,
+          },
+          populate: {
+            deliveryBrand: true,
+            user: {
+              // 상호명, 개업일자
+              select: ["businessName", "startDate", "id"],
+            },
+            deliveryLocations: {
+              select: ["id", "SIDO", "SIGUNGU", "EUPMEONDONG"],
+            },
+            deliveryAgencyImages: {
+              select: ["id", "url"],
+            },
+            documentOfInsurance: {
+              select: ["id", "url"],
+            },
+          },
+          ratePlan: true,
+        });
     },
 
     async getDeliveryAgencies(condition) {
       const deliveryAgencyRepository = strapi.db.query(
         "api::delivery-agency.delivery-agency"
       );
+      // 별점 계산해서 내보내주세요.
       return await deliveryAgencyRepository.findMany(condition);
     },
 
     /** 배달대행 업체 정보를 추가한다. */
-    async registerOrEdit({ deliveryAgencyInfo, user }) {
-      const deliveryAgencyRepository = strapi.db.query(
-        "api::delivery-agency.delivery-agency"
-      );
-      const exsist = await this.getDeliveryAgency({ user: user.id });
-      if (exsist) {
-        //** 이미 배달 대행 정보가 등록이 되어있는 경우 */
-        return await deliveryAgencyRepository.update({
-          data: deliveryAgencyInfo,
-          where: {
-            id: exsist.id,
-          },
-        });
-      }
-      /** 배달 대행 정보가 등록이 되어있지 않은 경우 */
-      return await strapi.entityService.create(
+    async updateInfo({ deliveryAgencyInfo, user, files }) {
+      // 배달대행정보를 업데이트시킨다.
+      return await strapi.entityService.update(
         "api::delivery-agency.delivery-agency",
+        exist.id,
         {
-          data: {
-            ...deliveryAgencyInfo,
-            user: user.id,
-            name: user.businessName,
-          },
+          data: deliveryAgencyInfo,
+          files,
         }
       );
     },
